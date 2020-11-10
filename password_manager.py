5715f76c3ec589032424fca2a840ca3ad7303d21
Charles-Mardis <72397256+Charles-Mardis@users.noreply.github.com>
2020-11-10 10:38:47 -0600

    Save is complete

--- password_manager.py
+++ password_manager.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect('information.db')
 cur = conn.cursor()
 
-cur.execute(""" CREATE TABLE IF NOT EXISTS accounts(
+cur.execute(""" CREATE TABLE IF NOT EXISTS accountinfo(
     Username TEXT,
     Email TEXT,
     Password TEXT
@@ -23,14 +23,21 @@ while True:
         username = input("What woud you like your user name to be? ")
         email = input("What's your email? ")
         password_first = input("What would you like your password to be? ")
-        password_same = input("Please reenter your password. ")
-        if password_first == password_same:
-            password = password_first
-            break
-        else:
-            print("Passwords do not match please try agian. ")
+        while True:
+            password_same = input("Please reenter your password. ")
+            if password_first == password_same:
+                password = password_first
+                break
+            else:
+                print("Passwords do not match please try again. ")
+    break
 
-# elif answer == 'login':
+cur.execute(""" INSERT INTO accountinfo (Username, Email, Password) VALUES (?, ?, ?)""", (username, email, password))
+conn.close()
+elif answer == 'login':
+    username_validation = input("Username: ")
+    password_validation = input("Password: ")
+    cur.execute(""" SELECT * FROM accountinfo WHERE Username = )
 
 
 # elif answer == 'exit':
